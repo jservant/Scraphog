d9125e9c42a7c0e8aa3b7b347b6aa15898aff862
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2020-02-21 01:07:02 -0500

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public Vector2 direction;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = direction;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public int dmgLevel;
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(moveH, moveV);
+        rb.velocity = move * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+    }
+}

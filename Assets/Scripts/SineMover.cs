127289dc7619f38bf23e3c4dc1a73e853f992889
mynameisfloog <48299534+mynameisfloog@users.noreply.github.com>
2020-02-23 17:14:58 -0500

    Sine movement on EnemyS

---
+++ Assets/Scripts/SineMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineMover : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed; // Vertical movement speed
+
+    [SerializeField]
+    float frequency; // How often object goes left and right
+
+    [SerializeField]
+    float magnitude; // The highest/lowest points the object will go in the wave
+
+    Vector3 pos;
+    
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        pos -= transform.up * Time.deltaTime * moveSpeed;
+        transform.position = pos + transform.right * Mathf.Sin(Time.time * frequency) * magnitude;
+    }
+}
